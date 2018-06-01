489792ccfa7fe03b27ba83017c4b57574519b026
Alin <alinpaunescu@gmail.com>
2018-05-31 23:06:56 -0700

    1 done

--- exer3.hs
+++ exer3.hs
@@ -1,1 +1,26 @@
-#exer3.hs
+merge :: Ord a => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge (x:xs) (y:ys)
+	| x >= y = (y : merge ys (x:xs))
+	| y > x = (x : merge xs (y:ys))
+
+
+hailLen 1 = 0
+hailLen n = hailTail 1 n
+	where 
+		hailTail a _ = _
+		hailTail a _ = hailTail _ _
+
+
+hailLen 1 = 0
+hailLen n
+    |n `mod` 2 == 0 = hailLen(n`div` 2) + 1
+    |n `mod` 2 /= 0 = hailLen(3*n + 1) + 1 
+
+
+
+
+join n [] = []
+join n (a:[]) = a
+join n (x:xs) = x ++ n ++ (join n xs)
